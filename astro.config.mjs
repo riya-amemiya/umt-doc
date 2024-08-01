c924cef7fcac6682366eab44decc029ea51ee103
Riya Amemiya <riyaamemiya@gmail.com>
2024-08-02 03:05:10 +0900

    feat: Astroの設定にsitemapとpartytownの統合を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), sitemap(), partytown()],
   output: "server",
   adapter: vercel(),
 });
